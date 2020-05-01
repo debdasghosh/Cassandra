2f9f249a08778f938a95ac80dab0f3e16f27b983
Debdas Ghosh <89debdas@gmail.com>
2020-04-30 21:38:21 -0400

    Add files via upload

---
+++ mini-project-03/src/main/java/LogAnalysis1.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package log;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Session;
+
+public class LogAnalysis1 {
+
+    public void pathHits() {
+        Cluster cluster = null;
+        try {
+            cluster = Cluster.builder().addContactPoint("134.209.160.211").build();
+            Session session = cluster.connect();
+
+            ResultSet resultSet = session.execute("select count(*) from project_03.log where path='/assets/img/release-schedule-logo.png' ALLOW FILTERING;");
+
+            System.out.println("/assets/img/release-schedule-logo.png " + resultSet.one().getLong("count"));
+
+        } finally {
+            if (cluster != null)
+                cluster.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        new LogAnalysis1().pathHits();
+        long end = System.currentTimeMillis();
+        double total = (end - start) / 1000.0;
+        System.out.println("Total running time: " + total + " seconds");
+    }
+
+}
---
+++ mini-project-03/src/main/java/LogAnalysis3.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package log;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+
+public class LogAnalysis3 {
+
+    public void pathHits() {
+        Cluster cluster = null;
+        try {
+            cluster = Cluster.builder().addContactPoint("134.209.160.211").build();
+            Session session = cluster.connect();
+
+            ResultSet resultSet = session.execute("SELECT * from project_03.path;");
+            String path = "";
+            long count = 0;
+            for (Row row : resultSet) {
+                long rowCount = row.getLong("count");
+                if (rowCount > count) {
+                    count = rowCount;
+                    path = row.getString("path");
+                }
+            }
+            System.out.println(path + " " + count);
+
+        } finally {
+            if (cluster != null)
+                cluster.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        new LogAnalysis3().pathHits();
+        long end = System.currentTimeMillis();
+        double total = (end - start) / 1000.0;
+        System.out.println("Total running time: " + total + " seconds");
+    }
+
+}
